efb55aac42263242eace212a02775865a5e4e8a3
Grzegorz Kostalkowicz <g.kostalkowicz@gmail.com>
2018-02-25 12:14:29 +0000

    Routes and navigation

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,11 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { RestaurantRowComponent } from './restaurant-row/restaurant-row.component';
 import { OverviewScreenComponent } from './overview-screen/overview-screen.component';
 import { VotingScreenComponent } from './voting-screen/voting-screen.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'overview', pathMatch: 'full' },
+  { path: 'overview', component: OverviewScreenComponent },
+  { path: 'vote', component: VotingScreenComponent }
+]
 
 @NgModule({
   declarations: [
@@ -18,7 +25,8 @@ import { VotingScreenComponent } from './voting-screen/voting-screen.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
