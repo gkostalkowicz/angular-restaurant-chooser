00c7e6b13114a74de57c323c19b3639c0edd58cb
Grzegorz Kostalkowicz <g.kostalkowicz@gmail.com>
2018-03-03 11:51:20 +0000

    Add restaurant form

--- client/src/app/manage-restaurants-create-screen/manage-restaurants-create-screen.component.ts
+++ client/src/app/manage-restaurants-create-screen/manage-restaurants-create-screen.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, AbstractControl, Validators } from '@angular/forms';
+import { Response } from '@angular/http';
+import { Router, ActivatedRoute } from '@angular/router';
+import { RestService  } from '../rest.service';
 
 @Component({
   selector: 'app-manage-restaurants-create-screen',
@@ -7,9 +11,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManageRestaurantsCreateScreenComponent implements OnInit {
 
-  constructor() { }
+  formGroup: FormGroup;
+  name: AbstractControl;
+
+  constructor(builder: FormBuilder, private restService: RestService, private router: Router, private route: ActivatedRoute) {
+    this.formGroup = builder.group({
+      'name': ['', Validators.required]
+    });
+    this.name = this.formGroup.controls['name'];
+  }
 
   ngOnInit() {
   }
 
+  onSubmit(formGroup: FormGroup) {
+    this.restService
+      .post('/restaurants', {name: formGroup['name']})
+      .subscribe((response: Response) => {
+        this.router.navigate(['..'], { relativeTo: this.route });
+      });
+  }
 }
